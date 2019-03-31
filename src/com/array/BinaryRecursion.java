4806bacd773fab3325eec308a1f7f2d3f3f6f9c4
Prateek-Chhibber <0392pc@gmail.com>
2019-03-31 17:11:17 +0530

    Update BinaryRecursion.java
    Final Version with return statement.

--- src/com/array/BinaryRecursion.java
+++ src/com/array/BinaryRecursion.java
@@ -7,7 +7,8 @@ public class BinaryRecursion {
 		int arr[] = { 2, 3, 4, 10, 40 };
 		int x = 10;
 		int start = 0;
-		int end = arr.length - 1; 
+		int end = arr.length - 1;
+		System.out.println("end = " + end);
 		int result = binaryRecursive(arr,start,end,x);
 		if(result == -1) {
 			System.out.println("Element not present");
@@ -19,7 +20,7 @@ public class BinaryRecursion {
 	public static int binaryRecursive(int arr[],int start, int end, int find) {
 		int mid = 0;
 		if(start<=end) {
-			mid = (start + end) /2;
+			mid = (start + end)/2;
 			System.out.println("mid = " + mid);
 			if(arr[mid] == find) {
 				return mid;
@@ -27,6 +28,7 @@ public class BinaryRecursion {
 //			Check if element is greater than middle then ignore left half
 			else if(find>arr[mid]) {
 				binaryRecursive(arr, mid+1, end, find);
+				return binaryRecursive(arr, mid+1, end, find);
 			}
 //			Check if element is greater than middle then ignore right half
 			else {
