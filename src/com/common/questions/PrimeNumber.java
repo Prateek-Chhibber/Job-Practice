a017b7c5173349cc6018f9416a29437fd8bfa4ce
Prateek-Chhibber <0392pc@gmail.com>
2019-03-31 15:51:45 +0530

    #Change-1
    Name is changed in local repository, thus cloning again to master with new packagecommon.questions

---
+++ src/com/common/questions/PrimeNumber.java
@@ -0,0 +1,32 @@
+package com.common.questions;
+
+public class PrimeNumber {
+	
+	static int i = 2;
+	static boolean flag = false;
+	
+	public static void main(String[] args) {
+		
+		primeNumber(9);
+		
+	}
+	
+	public static void primeNumber(int num) {
+		
+		while(i<=num/2) {
+			
+			//Condition for non prime number
+			if(num% i==0) {
+				flag = true;
+				break;
+			}
+			++i;
+		}
+		if(!flag) {
+			System.out.println(num + " is a prime no. ");
+		}else {
+			System.out.println(num + " is not a prime no. ");
+		}
+	}
+
+}
